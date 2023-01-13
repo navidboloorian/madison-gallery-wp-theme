d54de4b67c4f5cc6e3cda390053c5f32e0228c1e
Navid Boloorian <navidboloorian0@gmail.com>
2023-01-12 16:54:54 -0800

    styled navbar

--- front-page.php
+++ front-page.php
@@ -8,10 +8,19 @@
     </head>
     <body>
         <div id="main-wrapper">
-            <nav>
-                <!--unblurred this image as a QOL fix from the original website-->
-                <img width="256" src=<?php echo get_template_directory_uri()."/assets/images/navbar-logo.jpg" ?> />
-            </nav>
+            <header id="header-wrapper">
+                <nav id="navbar">
+                    <!--unblurred this image as a QOL fix from the original website-->
+                    <a href="#"><img width="235" src=<?php echo get_template_directory_uri()."/assets/images/navbar-logo.jpg" ?> /></a>
+                    <ul id="navbar-directory">
+                        <a href="#"><li>ARTISTS</li></a>
+                        <a href="#"><li>EXHIBTIONS</li></a>
+                        <a href="#"><li>PRESS</li></a>
+                        <a href="#"><li>CONTACT</li></a>
+                        <a href="#"><li>ABOUT</li></a>
+                    </ul>
+                </nav>
+            </header>
             <?php 
                 // inject scripts
                 wp_footer();
