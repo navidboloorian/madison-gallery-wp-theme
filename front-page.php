8b6a1759b25fc1b76deb502df455b8434af9d10a
Navid Boloorian <navidboloorian0@gmail.com>
2023-01-12 18:48:58 -0800

    created artsits db and called it for list of artists in footer

--- front-page.php
+++ front-page.php
@@ -21,10 +21,42 @@
                     </ul>
                 </nav>
             </header>
-            <?php 
-                // inject scripts
-                wp_footer();
-            ?>
         </div>
+        <footer id="footer">
+            <div id="footer-content">
+                <div class="footer-row" id="footer-contact-row">
+                    <div id="footer-address">
+                        <p>
+                            320 SOUTH CEDROS AVENUE, SUITE 200<br>
+                            SOLANA BEACH, CA 92075<br>
+                            TEL 858 523 9155<br>
+                            INFO@MADISONGALLERIES.COM
+                        </p>
+                    </div>
+                    <div id="footer-email">
+                        <p>
+                            To receive emails about updates, exhibitions, and more, sign up for our mailing list.
+                        </p>
+                    </div>
+                    <div id="footer-links"></div>
+                </div>
+                <div class="footer-row" id="footer-artists-row">
+                    <?php 
+                        global $wpdb;
+
+                        $artists = $wpdb->get_results('SELECT * FROM artists');
+                        
+                        foreach ($artists as &$artist) {
+                            echo '<a href="#"><li class="footer-artist-name">'.$artist->name.'</li></a>';
+                        }
+                    ?>
+                </div>
+                <div class="footer-row" id="footer-copyright-row">test</div>
+            </div>
+        </footer>
+        <?php 
+            // inject scripts
+            wp_footer();
+        ?>
     </body>
 </html>
