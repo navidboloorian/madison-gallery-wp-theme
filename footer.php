f595c10bb8e02a5aacd483366f29339efbfdf8b0
Navid Boloorian <navidboloorian0@gmail.com>
2023-01-29 19:48:44 -0800

    updated footer artist source

--- footer.php
+++ footer.php
@@ -18,18 +18,26 @@
                 </div>
                 <div class="footer-row" id="footer-artists-row">
                     <?php 
-                        $artists_args = array(
-                            'category_name' => 'artist',
-                            'orderby' => 'name',
+                        $args = array(
+                            'post_type' => 'artist',
+                            'orderby' => 'title',
                             'order' => 'ASC',
                             'numberposts' => '-1',
                         );
-
-                        $artists = get_posts($artists_args);
-
-                        foreach ($artists as &$artist) {
-                            echo '<a href="'.$artist->guid.'"><li class="footer-artist-name">'.$artist->post_title.'</li></a>';
+                
+                        $loop = new WP_Query($args);
+                
+                        while ($loop->have_posts()) {
+                            $loop->the_post();
+                
+                            $artistUrl = get_the_permalink();
+                            $artistName = get_the_title();
+                            $imgUrl = get_the_post_thumbnail_url();
+                
+                            echo '<a href="'.$artistUrl.'"><li class="footer-artist-name">'.$artistName.'</li></a>';
                         }
+                
+                        wp_reset_postdata();
                     ?>
                 </div>
                 <div class="footer-row" id="footer-copyright-row">
